fa643c64f809c67433dd629d45e0383650dcba41
fippy <davegilfillan@hotmail.com>
2022-03-18 13:54:22 +0000

    Updates

---
+++ precache-manifest.49bb2faeedc8f3ba25cfe385556043e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35cb63a64714d928a401",
+    "url": "/course-directory/static/css/main.8c776619.chunk.css"
+  },
+  {
+    "revision": "35cb63a64714d928a401",
+    "url": "/course-directory/static/js/main.35cb63a6.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "fc6b38254c87cfefd1685fe513950778",
+    "url": "/course-directory/index.html"
+  }
+];
